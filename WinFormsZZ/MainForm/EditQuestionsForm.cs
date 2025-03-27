96ff91cb05d416fa033e1927f3ce874ce2b510ed
Gavrilchenko-Stepan <atom47228@gmail.com>
2025-03-27 11:39:43 +0300

    Добавил метод InitCategories для добавления категорий в checkedListBox1 и метод UpdateCurrentQuestions для обновления текущих вопросов

--- WinFormsZZ/MainForm/EditQuestionsForm.cs
+++ WinFormsZZ/MainForm/EditQuestionsForm.cs
@@ -13,12 +13,52 @@ namespace MainForm
 {
     public partial class EditQuestionsForm: Form
     {
+        private List<Question> questions;
+
+        private string[] categories = { "знать", "уметь", "владеть" };
+
+        private string selectedCategory;
+
+        private List<Question> currentQuestions;
         public EditQuestionsForm()
         {
             InitializeComponent();
         }
+
+        private void InitCategories()
+        {
+            // Заполняем CheckedListBox категориями
+            checkedListBox1.Items.Clear();
+            foreach (var category in categories)
+            {
+                checkedListBox1.Items.Add(category, false);
+            }
+        }
+
         private void checkedListBox1_SelectedIndexChanged(object sender, EventArgs e)
         {
+            selectedCategory = checkedListBox1.CheckedItems.Cast<string>().FirstOrDefault();
+            UpdateCurrentQuestions(selectedCategory);
+        }
+
+        private void UpdateCurrentQuestions(string category)
+        {
+            currentQuestions = new List<Question>();
+
+            if (category != null)
+            {
+                foreach (var question in questions)
+                {
+                    if (question.Section == category)
+                    {
+                        currentQuestions.Add(question);
+                    }
+                }
+            }
+            else
+            {
+                currentQuestions = questions.ToList();
+            }
         }
     }
 }
